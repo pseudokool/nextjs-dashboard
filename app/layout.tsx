efe1848077dc2c0bd8bf6fca794a6a1fe27820d3
pseudokool <carlyleoliver@gmail.com>
2024-03-26 00:11:29 +0530

    Added auth & meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://nextjs-dashboard-8537lerf0-psks-projects-1e7207c8.vercel.app'),
+};
+
 
 export default function RootLayout({
   children,
